112c176b68f1357a185907fc432ee0f05b46fc5d
Marco <mako.canto@gmail.com>
2021-03-14 19:33:54 -0700

    latest version (updating edit set name)

--- app/src/main/java/com/example/flashcardz/SetEditActivity.java
+++ app/src/main/java/com/example/flashcardz/SetEditActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.parse.ParseException;
+import com.parse.ParseUser;
+import com.parse.SaveCallback;
 
 public class SetEditActivity extends AppCompatActivity {
 
+    public static final String TAG = "EditSetActivity";
     EditText etSetName;
     Button btnSave;
 
@@ -28,16 +35,36 @@ public class SetEditActivity extends AppCompatActivity {
         btnSave.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent i = new Intent();
+                String setName = etSetName.getText().toString();
+                if (setName.isEmpty()) {
+                    Toast.makeText(SetEditActivity.this, "Set name can't be empty", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                ParseUser currentUser = ParseUser.getCurrentUser();
+                saveSet(setName, currentUser);
 
-                i.putExtra("setName", etSetName.getText().toString());
-                i.putExtra("position", getIntent().getExtras().getInt("position"));
-
-                setResult(RESULT_OK, i);
-
-
-                finish();
             }
         });
+    }
+
+        private void saveSet(String setName, ParseUser currentUser) {
+            Set set = new Set();
+            set.setSetName(setName);
+            set.setUser(currentUser);
+            set.saveInBackground(new SaveCallback() {
+                @Override
+                public void done(ParseException e) {
+                    if (e != null){
+                        Log.e(TAG, "ERROR while saving", e);
+                        Toast.makeText(SetEditActivity.this, "Error while saving!", Toast.LENGTH_SHORT).show();
+                    }
+                    Log.i(TAG, "Post save was successful!!");
+                    Intent i = new Intent();
+                    i.putExtra("setName", setName);
+                    setResult(RESULT_OK, i);
+//                goMainActivity();
+                    finish();
+                }
+            });
     }
 }
